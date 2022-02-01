94b583a2d44ca20b850ad371c1fa29615d3bd7ff
Breno Prata <brenoprata@hotmail.com>
2022-02-01 23:02:12 +0100

    Finish About me section

--- src/components/main/about-me/AboutMeDescription.tsx
+++ src/components/main/about-me/AboutMeDescription.tsx
@@ -3,10 +3,11 @@ export const AboutMeDescription = () => (
 		<i>*** Receiving transmission ***</i>
 		<span>
 			Brazilian, 26 years old, living in Czech Republic, video game nerd, pet lover and last, but not least,{' '}
-			<b>SOFTWARE DEVELOPER</b>. 👨‍💻
+			<b className={'test'}>SOFTWARE DEVELOPER</b>. 👨‍💻
 		</span>
 		<span>
-			On a space journey looking for a mothership that will take me to <b>infinity and beyond</b> ! 🚀
+			On a space journey looking for a mothership that will take me to{' '}
+			<b className={'infinity'}>infinity and beyond!</b> 🚀
 		</span>
 	</div>
 )
--- src/components/main/about-me/AboutMeImage.tsx
+++ src/components/main/about-me/AboutMeImage.tsx
@@ -3,7 +3,5 @@ export const AboutMeImage = () => (
 		<div className={'star-path-1'} />
 		<div className={'star-path-2'} />
 		<div className={'star-path-3'} />
-		<div className={'star-path-4'} />
-		<div className={'star-path-5'} />
 	</div>
 )
