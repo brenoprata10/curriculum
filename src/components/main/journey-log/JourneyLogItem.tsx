bd47853d295b4875647f82f14a67c1f22862e092
Breno Prata <brenoprata@hotmail.com>
2022-02-04 20:45:48 +0100

    Fix mobile layout

--- src/components/main/journey-log/JourneyLogItem.tsx
+++ src/components/main/journey-log/JourneyLogItem.tsx
@@ -2,10 +2,12 @@ import {IJourneyLogItemProps} from './JourneyLog'
 
 export const JourneyLogItem = ({id, name, role, date, image, ringConfig}: IJourneyLogItemProps) => (
 	<div className={'planet'}>
-		<img src={image} />
+		<div className={'planet-image'}>
+			<img src={image} />
+			<div className={`ring ring-${id}`} style={ringConfig} />
+		</div>
 		<span className={'planet-name'}>{name}</span>
 		<span className={'planet-role'}>{role}</span>
 		<span className={'planet-date'}>{date}</span>
-		<div className={`ring ring-${id}`} style={ringConfig} />
 	</div>
 )
