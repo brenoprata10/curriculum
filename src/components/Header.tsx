a18a329aa40a99fa9ca5130ad38e8b483e449117
Breno Prata <brenoprata@hotmail.com>
2022-02-06 00:26:16 +0100

    Refactoring header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,6 @@ import {ExternalIcons} from './ExternalIcons'
 export const Header = () => {
 	return (
 		<header className={'header'}>
-			<span className={'name'}>Breno Prata</span>
 			<ExternalIcons />
 		</header>
 	)
