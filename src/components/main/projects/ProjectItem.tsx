4e4467b0c47f6cc3b18705fdfc40b1ef10d99364
Breno Prata <brenoprata@hotmail.com>
2022-02-09 13:12:45 +0100

    Add IBGE SQL project data

--- src/components/main/projects/ProjectItem.tsx
+++ src/components/main/projects/ProjectItem.tsx
@@ -1,3 +1,12 @@
 import {IProjectItemProps} from './Projects'
 
-export const ProjectItem = ({}: IProjectItemProps) => <span>Testing</span>
+export const ProjectItem = ({name, description, image, badges, goToCallback}: IProjectItemProps) => (
+	<div className={'project-item'}>
+		<div>
+			<img src={image} alt={name} />
+			<div>
+				<span>{name}</span> <span>{description}</span>
+			</div>
+		</div>
+	</div>
+)
--- src/components/main/projects/Projects.tsx
+++ src/components/main/projects/Projects.tsx
@@ -1,14 +1,36 @@
 import {faMugHot} from '@fortawesome/free-solid-svg-icons'
 import {Section} from '../../utils/Section'
+import {IBadge} from '../qualification/QualificationSection'
 import {ProjectItem} from './ProjectItem'
+import ANGULAR_BADGE from '../../../public/images/badges/front-end/angular-badge.svg'
+import TYPESCRIPT_BADGE from '../../../public/images/badges/front-end/typescript-badge.svg'
+import SASS_BADGE from '../../../public/images/badges/front-end/sass-badge.svg'
+import BRASIL_IMAGE from '../../../public/images/projects/brasil.svg'
 
 export interface IProjectItemProps {
 	id: string
 	name: string
+	description: string
+	badges: IBadge[]
 	image: string
+	goToCallback: () => void
 }
 
-const PROJECTS: IProjectItemProps[] = []
+const PROJECTS: IProjectItemProps[] = [
+	{
+		id: 'ibge-sql',
+		name: 'IBGE-SQL',
+		description:
+			'Developed to help other software developers and companies overall to populate their database with the most recent SQL script with all the states and cities of the country.',
+		badges: [
+			{path: ANGULAR_BADGE, name: 'Angular'},
+			{path: TYPESCRIPT_BADGE, name: 'Typescript'},
+			{path: SASS_BADGE, name: 'SASS'},
+		],
+		image: BRASIL_IMAGE,
+		goToCallback: () => window.open('https://ibge-sql.herokuapp.com/', '_newtab'),
+	},
+]
 
 export const Projects = () => (
 	<Section wrapperClassName={'projects'} title={'Projects'} icon={faMugHot}>
