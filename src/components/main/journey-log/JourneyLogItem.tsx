237b049c6980b56ea0b4a8a3c188e0bf19f6f89f
Breno Prata <brenoprata@hotmail.com>
2022-02-09 22:37:49 +0100

    Fix animation for safari

--- src/components/main/journey-log/JourneyLogItem.tsx
+++ src/components/main/journey-log/JourneyLogItem.tsx
@@ -1,10 +1,14 @@
 import {IJourneyLogItemProps} from './JourneyLog'
+import {isSafariBrowser} from '../../../utils/browser'
 
 export const JourneyLogItem = ({id, name, role, date, image, ringConfig}: IJourneyLogItemProps) => (
 	<div className={'planet'}>
 		<div className={'planet-image'}>
 			<img src={image} />
-			<div className={`ring ring-${id}`} style={ringConfig} />
+			<div
+				className={`ring ring-${id}`}
+				style={{...ringConfig, ...(isSafariBrowser() ? {animationDuration: '40s'} : {})}}
+			/>
 		</div>
 		<span className={'planet-name'}>{name}</span>
 		<span className={'planet-role'}>{role}</span>
