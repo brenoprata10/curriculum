121fe1a608ac1db2cc143feb411fb3fc0acb0fc2
Breno Prata <brenoprata@hotmail.com>
2022-02-10 22:17:30 +0100

    Remove material UI and other useless deps

--- src/components/main/journey-log/JourneyLogItem.tsx
+++ src/components/main/journey-log/JourneyLogItem.tsx
@@ -4,7 +4,7 @@ import {isSafariBrowser} from '../../../utils/browser'
 export const JourneyLogItem = ({id, name, role, date, image, ringConfig}: IJourneyLogItemProps) => (
 	<div className={'planet'}>
 		<div className={'planet-image'}>
-			<img src={image} />
+			<img src={image} alt={'rotating-planet'} />
 			<div
 				className={`ring ring-${id}`}
 				style={{...ringConfig, ...(isSafariBrowser() ? {animationDuration: '40s'} : {})}}
