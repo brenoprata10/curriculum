e15c0670a9fd5dc62d6f734dd84ee889ea4dcb7a
Breno Prata <brenoprata@hotmail.com>
2024-03-02 15:03:22 +0100

    Add QR Code to the PDF curriculum

--- src/components/pdf/index.tsx
+++ src/components/pdf/index.tsx
@@ -1,4 +1,4 @@
-import {Document, Page, StyleSheet, View} from '@react-pdf/renderer'
+import {Document, Page, StyleSheet, View, Image, Link} from '@react-pdf/renderer'
 import {PDF_THEME} from '../../utils/pdf-theme'
 import {EQualificationSection, QUALIFICATION_CONFIG} from '../main/qualification/Qualification'
 import Abilities from './abilities/Abilities'
@@ -8,6 +8,9 @@ import Education from './education/Education'
 import PDFHeader, {PDF_HEADER_HEIGHT} from './PDFHeader'
 import Projects from './projects/Projects'
 import WorkExperience from './work-experience/WorkExperience'
+import QR_CODE from '../../public/images/curricullum-qr-code.png'
+import PDFTitle from '../utils/PDFTitle'
+import PDFSection from '../utils/PDFSection'
 
 const LEFT_CONTAINER_WIDTH = 35
 const PAGE_HEIGHT = 841.7
@@ -26,16 +29,27 @@ const CurriculumPDF = () => (
 			<PDFHeader />
 			<View style={styles.container}>
 				<View style={styles.leftContainer}>
-					<Contact />
-					<Education />
-					<Abilities
-						title={'Front-end Tech'}
-						badges={QUALIFICATION_CONFIG[EQualificationSection.FRONT_END].badges}
-						minHeight={341}
-					/>
-					<Abilities title={'Back-end Tech'} badges={QUALIFICATION_CONFIG[EQualificationSection.BACK_END].badges} />
-					<Abilities title={'Testing Tech'} badges={QUALIFICATION_CONFIG[EQualificationSection.TESTING].badges} />
-					<Abilities title={'Design Tech'} badges={QUALIFICATION_CONFIG[EQualificationSection.DESIGN].badges} />
+					<View>
+						<Contact />
+						<Education />
+						<Abilities
+							title={'Front-end Tech'}
+							badges={QUALIFICATION_CONFIG[EQualificationSection.FRONT_END].badges}
+							minHeight={341}
+						/>
+						<Abilities title={'Back-end Tech'} badges={QUALIFICATION_CONFIG[EQualificationSection.BACK_END].badges} />
+						<Abilities title={'Testing Tech'} badges={QUALIFICATION_CONFIG[EQualificationSection.TESTING].badges} />
+						<Abilities title={'Design Tech'} badges={QUALIFICATION_CONFIG[EQualificationSection.DESIGN].badges} />
+					</View>
+					<PDFSection>
+						<PDFTitle>Online CV</PDFTitle>
+						<View style={styles.qrCodeContainer}>
+							<Image src={QR_CODE} style={styles.qrCode} />
+							<Link src={'https://brenoprata10.github.io/curriculum'} style={styles.curriculumLink}>
+								Curriculum Link
+							</Link>
+						</View>
+					</PDFSection>
 				</View>
 				<View style={styles.rightContainer}>
 					<AboutMe />
@@ -64,8 +78,20 @@ const styles = StyleSheet.create({
 		width: `${LEFT_CONTAINER_WIDTH}%`,
 		backgroundColor: PDF_THEME.ternaryColor,
 		height: `${PAGE_HEIGHT - PDF_HEADER_HEIGHT + PAGE_HEIGHT}`,
+		justifyContent: 'space-between',
 	},
 	rightContainer: {
 		width: `${100 - LEFT_CONTAINER_WIDTH}%`,
+	},
+	qrCodeContainer: {
+		marginBottom: 40,
+		gap: 5,
+	},
+	qrCode: {
+		width: 120,
+		height: 120,
+	},
+	curriculumLink: {
+		marginLeft: 15,
 	},
 })
